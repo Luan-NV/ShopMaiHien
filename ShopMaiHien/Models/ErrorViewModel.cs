6c6275470723ca0c65c6528ec1aaa3edb226830a
LuanNV <luanvne@outlook.com>
2018-07-23 11:30:49 +0700

    Add project files.

---
+++ ShopMaiHien/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShopMaiHien.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
