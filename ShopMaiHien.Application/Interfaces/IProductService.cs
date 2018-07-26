1798b129626e9b045dd81702076b2505886ec2f3
LuanNV <luanvne@outlook.com>
2018-07-26 13:57:00 +0700

    paging page list of product

--- ShopMaiHien.Application/Interfaces/IProductService.cs
+++ ShopMaiHien.Application/Interfaces/IProductService.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 using ShopMaiHien.Application.ViewModels.Product;
+using ShopMaiHien.Utilities.Dtos;
 
 namespace ShopMaiHien.Application.Interfaces
 {
     public interface IProductService : IDisposable
     {
         List<ProductViewModel> GetAll();
+
+        PagedResult<ProductViewModel> GetAllPaging(int? categoryId, string keyword, int page, int pageSize);
     }
 }
